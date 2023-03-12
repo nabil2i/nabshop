607a6c0f00e5e10f07f985085e28f793b8649b58
Nabil <nabil.noble01@gmail.com>
2023-03-12 23:20:08 +0000

    create frontend vue app

---
+++ nabshopvue/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="wrapper">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <div class="container">
+        <router-link to="/">
+        
+        </router-link>
+      </div>
+    </nav>
+  <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
