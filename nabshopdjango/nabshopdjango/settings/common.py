59ace094e6f0783bc71388b1f931f517bc6d5665
Nabil <nabil.noble01@gmail.com>
2023-03-12 18:38:50 +0000

    create the 'core' app and configure a model to extend AbstractUser to customize User for authentication

--- nabshopdjango/nabshopdjango/settings/common.py
+++ nabshopdjango/nabshopdjango/settings/common.py
@@ -36,7 +36,10 @@ INSTALLED_APPS = [
     'rest_framework',
     'debug_toolbar',
     
+    
     'store',
+    
+    'core',
     
 ]
 
@@ -119,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
