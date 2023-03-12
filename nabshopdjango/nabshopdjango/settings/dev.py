757e3dc8bd2e6282bdecec04c5fe8f6fce63b5a7
Nabil <nabil.noble01@gmail.com>
2023-03-12 17:07:14 +0000

    create store app and store/urls.py

--- nabshopdjango/nabshopdjango/settings/dev.py
+++ nabshopdjango/nabshopdjango/settings/dev.py
@@ -14,7 +14,10 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nabshop',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'rootnabil'
     }
 }
